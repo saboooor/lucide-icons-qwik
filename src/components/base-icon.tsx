a26b453fbd4ee4aa159f01432254d660b477222b
saboooor <saboor547@gmail.com>
2026-03-12 02:21:14 -0400

    uhh

--- src/components/base-icon.tsx
+++ src/components/base-icon.tsx
@@ -1,9 +1,9 @@
-import { component$, inlinedQrl, Slot } from '@qwik.dev/core';
+import { component$, Slot } from '@qwik.dev/core';
 
 import { BaseIconProps } from '../icon-props';
 import fallbackProps from '../default-props';
 
-export default inlinedQrl(component$(({
+export default component$(({
   name,
   size = fallbackProps.size,
   color = fallbackProps.color,
@@ -22,4 +22,4 @@ export default inlinedQrl(component$(({
   >
     <Slot/>
   </svg>;
-}), 'lucide-icon');
+});
